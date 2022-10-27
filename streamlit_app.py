7495cacd317bea3f9325791206367c3816642792
mBajon <31763459+mBajon@users.noreply.github.com>
2022-10-27 21:28:31 +0200

    add headers and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('my parenst new healthy diner')
+streamlit.header('breakfast menu')
+streamlit.text('omega 3 & blueberry omlet')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard boiled free range egg')
