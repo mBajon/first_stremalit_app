5da43ec08f432fe473269cc7a0b1d9ed02b901bf
mBajon <31763459+mBajon@users.noreply.github.com>
2022-10-27 21:30:25 +0200

    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('my parenst new healthy diner')
 streamlit.header('breakfast menu')
-streamlit.text('omega 3 & blueberry omlet')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard boiled free range egg')
+streamlit.text('🥣 omega 3 & blueberry omlet')
+streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
+streamlit.text('🐔 Hard boiled free range egg')  
+streamlit.text('🥑🍞 Avocado toast
+')  
