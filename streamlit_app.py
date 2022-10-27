d03cf426c4eed460ca6e916a4e06a7a5563f48f1
mBajon <31763459+mBajon@users.noreply.github.com>
2022-10-27 22:16:34 +0200

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 
 fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-
+fruit_list = fruit_list.set_index('Fruit')
 
 
 streamlit.title('my parenst new healthy diner')
